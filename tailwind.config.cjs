01dc73d7218448620f62dc68292594502e3acdb8
asheikho <105573050+asheikho99@users.noreply.github.com>
2022-12-24 23:08:41 -0400

    Theme colours

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,24 @@ module.exports = {
     './src/**/*.{html,js,svelte,ts}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'home': {
+          'primary': '#005dab',
+          'secondary': '#337cc1',
+          'highlight': '#1147a9'
+        },
+        'away': {
+          'primary': '#ea8211',
+          'secondary': '#ffac38',
+          'hightlight': '#e57215'
+        },
+        'theme': {
+          'primary': '#0a1624',
+          'secondary': '#1d2c3f'
+        }
+      },
+    },
   },
   plugins: [],
 }
