da87fdc4660b63aafb270923d19b2e645cefdf22
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-08-26 13:54:30 -0300

    Websocket connection handling

---
+++ src/lib/websocket.ts
@@ -0,0 +1,41 @@
+import { readonly, writable, type Writable } from 'svelte/store';
+
+interface WsStore {
+	websocket: undefined | WebSocket;
+	isConnected: boolean;
+	isDebug?: boolean;
+}
+
+export const wsWritableStore: Writable<WsStore> = writable({
+	websocket: undefined,
+	isConnected: false,
+	isDebug: false
+});
+
+export const createWsConnection = () => {
+	try {
+		wsWritableStore.set({
+			websocket: new WebSocket('ws://localhost:49122'),
+			isConnected: true,
+			isDebug: false
+		});
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+export const destoryWsConnection = () => {
+	try {
+		wsStore.subscribe((values) =>
+			values.websocket?.close(1001, 'CONNECTION FORCIBLY CLOSED BY ROOT PAGE UNMOUNT')
+		);
+		wsWritableStore.set({
+			websocket: undefined,
+			isConnected: false
+		});
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+export const wsStore = readonly(wsWritableStore);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script lang="ts">
+	import { createWsConnection, destoryWsConnection, wsStore } from '$lib/websocket';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		createWsConnection();
+		wsStore.subscribe((values) => {
+			console.log(values.websocket);
+		});
+
+		return () => destoryWsConnection();
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
