b37758aa4dd5adcbae472e060d0c6a5eb7a76cf8
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-08-29 14:31:40 -0300

    components alias

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,6 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
@@ -14,7 +13,8 @@ const config = {
 		adapter: adapter(),
 		alias: {
 			enums: './src/enums',
-			events: './src/events'
+			events: './src/events',
+			components: './src/components',
 		}
 	}
 };
