ce9ddbb7cb48be1c24eb97a0aee675644cc20806
asheikho99 <105573050+asheikho99@users.noreply.github.com>
2023-10-19 16:35:37 -0300

    Debug Ball Tab

--- src/components/Ball.svelte
+++ src/components/Ball.svelte
@@ -1,1 +1,25 @@
-<p>Ball</p>
+<script lang="ts">
+	import { updateStateStore } from './../events/on-update-state';
+	$: BALL = $updateStateStore.game.ball;
+</script>
+
+<div class="flex flex-col space-y-4">
+	<div class="flex flex-col">
+		<p class="font-semibold text-lg">Location</p>
+		<div class="flex flex-col space-y-2">
+			<p>X: {BALL.location.X}</p>
+			<p>Y: {BALL.location.Y}</p>
+			<p>Z: {BALL.location.Z}</p>
+		</div>
+	</div>
+
+	<div class="flex flex-col">
+		<p class="font-semibold text-lg">Last Hit</p>
+		<p>{BALL.team == 0 ? 'Blue Team' : 'Orange'}</p>
+	</div>
+
+	<div class="flex flex-col">
+		<p class="font-semibold text-lg">Speed</p>
+		<p>{BALL.speed}</p>
+	</div>
+</div>
