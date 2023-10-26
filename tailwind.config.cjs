42ee5712bfce0eff053615e30a8e1453e83af6ff
asheikho99 <105573050+asheikho99@users.noreply.github.com>
2023-10-25 22:25:34 -0300

    Statfeed event tailwind animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,33 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				foreground: '#2c2b31',
+				'foreground-muted': '#181a21',
+				'foreground-dark': '#141b22',
+				'foreground-light': '#2c2a30',
+				'team-0': {
+					primary: '#0182c5',
+					secondary: '#185571',
+					charge: '#1085d1',
+				},
+				'team-1': {
+					primary: '#f76d1f',
+					secondary: '#9f4829',
+					charge: '#f87a32',
+				},
+			},
+			animation: {
+				fade: 'fadeOut 1s ease-out 1',
+			},
+			keyframes: (theme) => ({
+				fadeOut: {
+					'0%': {transform: 'scale(1)', opacity: '100%' },
+					'100%': {transform: 'scale(2)', opacity: '0%' },
+				},
+			}),
+		},
 	},
 
 	plugins: [],
