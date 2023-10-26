3b781923ae616c7f1e0035e66be255a75b281138
asheikho99 <105573050+asheikho99@users.noreply.github.com>
2023-10-25 22:34:36 -0300

    Rebase cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,7 @@
 	import { onMount } from 'svelte';
 	import { createWsConnection, destoryWsConnection, wsStore } from '$lib/websocket';
 	import { eventProcessor } from '$lib/event-processor';
-	import { websocketHasGame } from '$lib/stores';
-	import { updateStateStore } from 'events/on-update-state';
-	import Scoreboard from 'components/overlay/scoreboard/Container.svelte';
-	import ActivePlayerContainer from 'components/overlay/active-player/Container.svelte';
-	import PlayersListContainer from 'components/overlay/players/Container.svelte';
-
-	$: GAME = $updateStateStore.game;
+	
 	onMount(() => {
 		createWsConnection();
 		wsStore.subscribe((values) => {
@@ -17,13 +11,3 @@
 		return () => destoryWsConnection();
 	});
 </script>
-
-{#if $websocketHasGame}
-	<div class="min-h-screen">
-		<Scoreboard />
-		<PlayersListContainer />
-		{#if GAME.hasTarget}
-			<ActivePlayerContainer />
-		{/if}
-	</div>
-{/if}
--- src/routes/debug/+page.svelte
+++ src/routes/debug/+page.svelte
@@ -1,42 +1,7 @@
 <script lang="ts">
-	import Settings from './../../components/Settings.svelte';
-	import Ball from './../../components/Ball.svelte';
-	import Players from './../../components/Players.svelte';
 	import { onMount } from 'svelte';
 	import { createWsConnection, destoryWsConnection, wsStore } from '$lib/websocket';
 	import { eventProcessor } from '$lib/event-processor';
-	import Game from 'components/Game.svelte';
-	import Replay from 'components/Replay.svelte';
-	import StatFeed from 'components/StatFeed.svelte';
-	import { websocketHasGame } from '$lib/shared/stores';
-
-	const tabs = [
-		{ title: 'Game', content: 'Game' },
-		{ title: 'StatFeed', content: 'StatFeed' },
-		{ title: 'Players', content: 'Players' },
-		{ title: 'Replay', content: 'Replay' },
-		{ title: 'Ball', content: 'Ball' },
-		{ title: 'Settings', content: 'Settings' },
-	];
-
-	const components: {
-		[key: string]:
-			| typeof Game
-			| typeof StatFeed
-			| typeof Players
-			| typeof Replay
-			| typeof Ball
-			| typeof Settings;
-	} = {
-		Game,
-		StatFeed,
-		Players,
-		Replay,
-		Ball,
-		Settings,
-	};
-
-	let selectedTab: number = 0;
 
 	onMount(() => {
 		createWsConnection();
@@ -46,28 +11,3 @@
 		return () => destoryWsConnection();
 	});
 </script>
-
-<div class="flex flex-row overflow-x-scroll justify-start border-b border-b-slate-300">
-	{#each tabs as tab, index}
-		<button
-			tabindex={index}
-			class={`disabled:cursor-not-allowed disabled:text-gray-400 hover:cursor-pointer py-2 font-semibold min-w-[100px] hover:text-black ${
-				selectedTab == index
-					? 'text-black border-b-black border-b-2 disabled:border-none'
-					: 'text-gray-600'
-			}`}
-			on:click={() => {
-				selectedTab = index;
-			}}
-			disabled={!$websocketHasGame}
-		>
-			{tab.title}
-		</button>
-	{/each}
-</div>
-
-{#if $websocketHasGame}
-	<div class="flex flex-col p-4">
-		<svelte:component this={components[tabs[selectedTab].content]} />
-	</div>
-{/if}
