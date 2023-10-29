209b1b2d9dd96d90e610d99ad634ecbbce31a60c
asheikho99 <105573050+asheikho99@users.noreply.github.com>
2023-10-29 15:05:40 -0300

    Initial overlay components

---
+++ frontend/src/lib/overlay/index.ts
@@ -0,0 +1,3 @@
+export { default as Scoreboard } from './scoreboard/Scoreboard.svelte';
+export { default as ActivePlayer } from './active-player/ActivePlayer.svelte';
+export { default as PlayersList } from './playersList/PlayersList.svelte';
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,7 +2,8 @@
 	import { onMount } from 'svelte';
 	import { createWsConnection, destoryWsConnection, wsStore } from '$lib/websocket';
 	import { eventProcessor } from '$lib/event-processor';
-	
+	import { ActivePlayer, PlayersList, Scoreboard } from '$lib/overlay';
+
 	onMount(() => {
 		createWsConnection();
 		wsStore.subscribe((values) => {
@@ -11,3 +12,9 @@
 		return () => destoryWsConnection();
 	});
 </script>
+
+<div class="min-h-screen bg-red-100">
+	<Scoreboard />
+	<PlayersList />
+	<ActivePlayer />
+</div>
